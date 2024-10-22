cfbcf15e3135abd17b8ee7c1f0c243a66627128b
odeilly <odeilly@gmail.com>
2024-10-23 07:47:52 +0900

    title追加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+    title: 'VuePress サンプル',
     lang: 'ja',
     base: '/vuepress-sample/',
     bundler: viteBundler(),
