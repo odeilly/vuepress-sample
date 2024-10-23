c2c08758cc422f63e10894b9c997f979d86d63ba
odeilly <odeilly@gmail.com>
2024-10-23 09:37:27 +0900

    フォーマット変更

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,30 +1,29 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
-    title: 'VuePress サンプル',
-    lang: 'ja',
-    base: '/vuepress-sample/',
-    bundler: viteBundler(),
-    theme: defaultTheme({
-        navbar: [
-            {
-                text: "MySQL",
-                children: [
-                    {
-                        text: "MySQL",
-                        link: "/mysql.html"
-                    }
-                ],
-            },
-            {
-                text: "Home",
-                link: "/"
-            }
+  title: "VuePress サンプル",
+  lang: "ja",
+  base: "/vuepress-sample/",
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: "MySQL",
+        children: [
+          {
+            text: "MySQL",
+            link: "/mysql.html",
+          },
         ],
-    }),
+      },
+      {
+        text: "Home",
+        link: "/",
+      },
+    ],
+  }),
 
-
-    port: 9000
-})
+  port: 9000,
+});
